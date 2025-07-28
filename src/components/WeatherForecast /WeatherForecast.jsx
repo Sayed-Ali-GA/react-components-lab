9c1e12d776df90045ca4bf5e6572bc460769c0e7
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-07-28 16:25:45 +0300

    test files

---
+++ src/components/WeatherForecast /WeatherForecast.jsx
@@ -0,0 +1,13 @@
+const weatherForecasts = (weather) => {
+    const {day, img, imgAlt, conditions, time} = weather
+       return(
+        <li>
+             <h1>day</h1>
+        </li>
+       )
+
+}
+
+
+
+export default weatherForecasts
